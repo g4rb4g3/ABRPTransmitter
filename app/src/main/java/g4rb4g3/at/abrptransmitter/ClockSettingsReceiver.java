882e4f88b290788130f6b32b6dfc6faf2508211f
g4rb4g3 <g4rb4g3@gmail.com>
2019-03-04 19:12:45 +0100

    implemented clocksettings receiver
    opens engineering menu if 2 clicks on time in the status bar are made in 2 seconds.

---
+++ app/src/main/java/g4rb4g3/at/abrptransmitter/ClockSettingsReceiver.java
@@ -0,0 +1,23 @@
+package g4rb4g3.at.abrptransmitter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ClockSettingsReceiver extends BroadcastReceiver {
+  private static int mCalls = 0;
+  private static long mLastCall = 0;
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    if(System.currentTimeMillis() - mLastCall > 2000) {
+      mCalls = 0;
+    }
+    mLastCall = System.currentTimeMillis();
+    mCalls++;
+    if (mCalls == 2) {
+      mCalls = 0;
+      context.startActivity(context.getPackageManager().getLaunchIntentForPackage("com.lge.ivi.engineermode"));
+    }
+  }
+}
