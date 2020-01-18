a7c50375869f027c2dd6cb76d2de9b05325e3b01
g4rb4g3 <g4rb4g3@gmail.com>
2020-01-18 09:07:28 +0100

    wip: implement abrp web extension

--- app/src/main/java/g4rb4g3/at/abrptransmitter/MainActivity.java
+++ app/src/main/java/g4rb4g3/at/abrptransmitter/MainActivity.java
@@ -1,6 +1,7 @@
 package g4rb4g3.at.abrptransmitter;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextClock;
@@ -9,21 +10,28 @@ import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+import g4rb4g3.at.abrptransmitter.ui.main.AbrpGeckViewFragment;
 import g4rb4g3.at.abrptransmitter.ui.main.SectionsPagerAdapter;
+
+import static com.lge.ivi.view.IviKeyEvent.KEYCODE_TUNE_DOWN;
+import static com.lge.ivi.view.IviKeyEvent.KEYCODE_TUNE_UP;
+import static g4rb4g3.at.abrptransmitter.ui.main.SectionsPagerAdapter.TAB_TITLES;
 
 public class MainActivity extends AppCompatActivity {
 
   boolean mFullscreen = false;
+  SectionsPagerAdapter mSectionsPageAdapter;
+  TabLayout mTabs;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+    mSectionsPageAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
     ViewPager viewPager = findViewById(R.id.view_pager);
-    viewPager.setAdapter(sectionsPagerAdapter);
-    TabLayout tabs = findViewById(R.id.tabs);
-    tabs.setupWithViewPager(viewPager);
+    viewPager.setAdapter(mSectionsPageAdapter);
+    mTabs = findViewById(R.id.tabs);
+    mTabs.setupWithViewPager(viewPager);
 
     TextClock textClock = findViewById(R.id.textclock);
 
@@ -50,6 +58,27 @@ public class MainActivity extends AppCompatActivity {
       if ((visibility & View.SYSTEM_UI_FLAG_VISIBLE) == 0) {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
       }
+    }
+  }
+
+  @Override
+  public boolean dispatchKeyEvent(KeyEvent event) {
+    switch (event.getKeyCode()) {
+      case KEYCODE_TUNE_UP:
+      case KEYCODE_TUNE_DOWN:
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+          notifyFragment(event.getKeyCode());
+        }
+        return true;
+      default:
+        return super.dispatchKeyEvent(event);
+    }
+  }
+
+  private void notifyFragment(int keycode) {
+    int position = mTabs.getSelectedTabPosition();
+    if (TAB_TITLES[position] == R.string.tab_abrp_geckoview) {
+      ((AbrpGeckViewFragment) mSectionsPageAdapter.getFragment(position)).onKeyEvent(keycode);
     }
   }
 }
