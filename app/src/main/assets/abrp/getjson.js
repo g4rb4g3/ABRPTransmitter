b866eb3aa44e4f415b63b003ef688b7e6cf70969
Samuel Brucksch <samuel.brucksch@sap.com>
2020-01-29 10:46:04 +0100

    improve js impl

--- app/src/main/assets/abrp/getjson.js
+++ app/src/main/assets/abrp/getjson.js
@@ -3,13 +3,18 @@ var getJSON = $.getJSON;
 
 $.getJSON = function (e, t, n) {
   console.log(e);
-  if (e.indexOf("get_sharing_telemetry") == -1 && e.indexOf("get_vehicle_telemetry") == -1 && e.indexOf("get_user_telemetry") == -1) {
+  if (!e.includes("get_sharing_telemetry") && !e.includes("get_vehicle_telemetry") && !e.includes("get_user_telemetry")) {
     return getJSON.apply(this, arguments);
   }
 
   var deferred = $.Deferred();
   try {
     getJSON.apply(this, arguments).done(function (data) {
+      var res = data.result;
+      var tlm_age = (new Date()).getTime() / 1000 - res.utc;
+      if (tlm_age > 10 && abrptransmitterTlm != null) { // TODO: find good value here, ABRP assumes > 90s is bad but that would cause slow UI updates. We update each 5s, so at least two tlm sets should come in here
+        Object.assign(data.result, abrptransmitterTlm); // this will overwrite the remote result with local data
+      }
       deferred.resolve(data)
     }).fail(function () {
       if (abrptransmitterTlm == null) {
